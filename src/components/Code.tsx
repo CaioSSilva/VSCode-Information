6e53cab02dd95d848936e5a96dcc7fe4dcb267f4
CaioSSilva <caiosouzasilva13650@gmail.com>
2023-04-02 15:10:26 -0300

    Pretty

--- src/components/Code.tsx
+++ src/components/Code.tsx
@@ -1,18 +1,18 @@
 import { JetBrains_Mono } from "next/font/google";
 
-const JetBrainsMono = JetBrains_Mono({subsets:['latin']})
+const JetBrainsMono = JetBrains_Mono({ subsets: ["latin"] });
 
-interface CodeProps{
-    code: string
+interface CodeProps {
+  code: string;
 }
 
-export function Code ({code}:CodeProps){
-    return(
-        <div
-            id="shinki-code"
-            style={JetBrainsMono.style}
-            className=""
-            dangerouslySetInnerHTML={{__html:code}}
-        ></div>
-    )
+export function Code({ code }: CodeProps) {
+  return (
+    <div
+      id="shinki-code"
+      style={JetBrainsMono.style}
+      className=""
+      dangerouslySetInnerHTML={{ __html: code }}
+    ></div>
+  );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,18 @@
-import { LucideIcon, X } from "lucide-react"
-import Link from "next/link"
+import Link from "next/link";
+import { LucideIcon, X } from "lucide-react";
 
-interface HeaderProps{
-  name: string,
-  Icon: LucideIcon
+interface HeaderProps {
+  name: string;
+  Icon: LucideIcon;
 }
-export default function Header({name, Icon}:HeaderProps) {
+export default function Header({ name, Icon }: HeaderProps) {
   return (
     <div className="flex gap-2 pl-2 items-center bg-[#2f2b46] hover:bg-[#383353] p-1 cursor-pointer w-min">
-        <Icon size={16} className="stroke-white"/>
-        <p className="text-white text-xs">{name}</p>
-        <Link href={'/'}>
-            <X size={16} className="hover:bg-[#221f31] stroke-white"/>
-        </Link>
+      <Icon size={16} className="stroke-white" />
+      <p className="text-white text-xs">{name}</p>
+      <Link href={"/"}>
+        <X size={16} className="hover:bg-[#221f31] stroke-white" />
+      </Link>
     </div>
-  )
+  );
 }
